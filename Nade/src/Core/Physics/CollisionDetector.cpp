5635681daec75eb62fdf61044c8f30d6383e36dc
Shaurya Mittal <mittalshaurya004@gmail.com>
2022-06-02 13:57:20 +0530

    basic vertex-to-vertex vector calc

--- Nade/src/Core/Physics/CollisionDetector.cpp
+++ Nade/src/Core/Physics/CollisionDetector.cpp
@@ -1,16 +1,23 @@
 #include "CollisionDetector.h"
 #include <iostream>
+#include <glm/glm.hpp>
+
 namespace Nade{
   void CollisionDetector::CollisionDetector::Detect(GameObject& object1, GameObject& object2){
-        for(int i = 0; i < object1.GetData().model.data.Vsize; i=i+3){
-          std::cout<<i/3<<"x : "<<object1.GetData().model.data.mPosition.at(i)<<std::endl;
-          std::cout<<i/3<<"y : "<<object1.GetData().model.data.mPosition.at(i+1)<<std::endl;
-          std::cout<<i/3<<"z : "<<object1.GetData().model.data.mPosition.at(i+2)<<std::endl;
-        }
-        for(int i = 0; i < object2.GetData().model.data.Vsize; i=i+3){
-          std::cout<<i/3<<"x : "<<object2.GetData().model.data.mPosition.at(i)<<std::endl;
-          std::cout<<i/3<<"y : "<<object2.GetData().model.data.mPosition.at(i+1)<<std::endl;
-          std::cout<<i/3<<"z : "<<object2.GetData().model.data.mPosition.at(i+2)<<std::endl;
-        }
+    for(int i = 0; i < object1.GetData().model.data.Vsize; i=i+3){
+      float v1 = object1.GetData().model.data.mPosition.at(i);
+      float v2 = object1.GetData().model.data.mPosition.at(i+1);
+      float v3 = object1.GetData().model.data.mPosition.at(i+2);
+      glm::vec3 v = glm::vec3(v1, v2, v3);
+      glm::vec3 f(0,0,0);
+      for(int p = 0; p < object2.GetData().model.data.Vsize; p=p+3){
+        float v12 = object2.GetData().model.data.mPosition.at(p);
+        float v22 = object2.GetData().model.data.mPosition.at(p+1);
+        float v32 = object2.GetData().model.data.mPosition.at(p+2);
+        glm::vec3 vf = glm::vec3(v12, v22, v32);
+        f = (vf-v);
+        std::cout<<f.x << " " << f.y << " " << f.z << std::endl;
+      }
+    }
   }
 }

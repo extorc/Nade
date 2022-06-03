bfb0fc331e4fb5c36e73b2511e613eaee5d3d142
Shaurya Mittal <mittalshaurya004@gmail.com>
2022-06-03 14:49:46 +0530

    Brevity

--- Nade/src/Core/Physics/CollisionDetector.cpp
+++ Nade/src/Core/Physics/CollisionDetector.cpp
@@ -4,9 +4,7 @@
 #include <vector>
 
 namespace Nade{
-  void CollisionDetector::CollisionDetector::Detect(GameObject& object1, GameObject& object2){
-    std::vector<glm::vec3> triangle1;
-    std::vector<glm::vec3> triangle2;
+  void CollisionDetector::DeriveTriangles(GameObject& object1, GameObject& object2, std::vector<glm::vec3>& triangle1, std::vector<glm::vec3>& triangle2){
     for(int i = 0; i < object1.GetData().model.data.Vsize; i=i+3){
       std::vector<float> pos = object1.GetData().model.data.mPosition;
       float v1 = pos.at(i);
@@ -21,6 +19,12 @@ namespace Nade{
       float v3 = pos.at(i+2);
       triangle2.push_back(glm::vec3(v1, v2, v3));
     }
+  }
+
+  void CollisionDetector::CollisionDetector::Detect(GameObject& object1, GameObject& object2){
+    std::vector<glm::vec3> triangle1;
+    std::vector<glm::vec3> triangle2;
+    DeriveTriangles(object1, object2, triangle1, triangle2);
     glm::vec3 normal1 = glm::normalize(glm::cross(triangle1.at(1) - triangle1.at(0), triangle1.at(2) - triangle1.at(0)));
     glm::vec3 normal2 = glm::normalize(glm::cross(triangle2.at(1) - triangle2.at(0), triangle2.at(2) - triangle2.at(0)));
     std::cout<< normal1.x << " " << normal1.y << " " << normal1.z << std::endl;
--- Nade/src/Core/Physics/CollisionDetector.h
+++ Nade/src/Core/Physics/CollisionDetector.h
@@ -5,5 +5,6 @@ namespace Nade{
   class CollisionDetector{
   public:
     static void Detect(GameObject& object1, GameObject& object2);
+    static void DeriveTriangles(GameObject& object1, GameObject& object2, std::vector<glm::vec3>& triangle1, std::vector<glm::vec3>& triangle2);
   };
 }

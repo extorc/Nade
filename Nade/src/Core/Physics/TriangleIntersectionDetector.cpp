a6f0d2747eb72b4dcb7df54075549056bc986b6c
Shaurya Mittal <mittalshaurya004@gmail.com>
2023-02-13 09:31:06 +0530

    Minor change

--- Nade/src/Core/Physics/TriangleIntersectionDetector.cpp
+++ Nade/src/Core/Physics/TriangleIntersectionDetector.cpp
@@ -4,27 +4,21 @@
 #include <vector>
 
 namespace Nade{
-  void TraingleIntersectionDetector::DeriveTriangles(GameObject& object1, GameObject& object2, std::vector<glm::vec3>& triangle1, std::vector<glm::vec3>& triangle2){
-    for(int i = 0; i < object1.GetData().model.data.Vsize; i=i+3){
-      std::vector<float> pos = object1.GetData().model.data.mPosition;
+  void TraingleIntersectionDetector::DeriveTriangles(GameObject& object, std::vector<glm::vec3>& triangle){
+    for(int i = 0; i < object.GetData().model.data.Vsize; i=i+3){
+      std::vector<float> pos = object.GetData().model.data.mPosition;
       float v1 = pos.at(i);
       float v2 = pos.at(i+1);
       float v3 = pos.at(i+2);
-      triangle1.push_back(glm::vec3(v1, v2, v3));
-    }
-    for(int i = 0; i < object2.GetData().model.data.Vsize; i=i+3){
-      std::vector<float> pos = object2.GetData().model.data.mPosition;
-      float v1 = pos.at(i);
-      float v2 = pos.at(i+1);
-      float v3 = pos.at(i+2);
-      triangle2.push_back(glm::vec3(v1, v2, v3));
+      triangle.push_back(glm::vec3(v1, v2, v3));
     }
   }
 
   void TraingleIntersectionDetector::TraingleIntersectionDetector::Detect(GameObject& object1, GameObject& object2){
     std::vector<glm::vec3> triangle1;
     std::vector<glm::vec3> triangle2;
-    DeriveTriangles(object1, object2, triangle1, triangle2);
+    DeriveTriangles(object1, triangle1);
+    DeriveTriangles(object2, triangle2);
 
     for(int i = 0; i < 3; i++){
       triangle1.at(i) += object1.GetPosition();
